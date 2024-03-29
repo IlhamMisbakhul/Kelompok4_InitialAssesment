201cbaa4dd52f2eaad8a4d103528e75aa7d56297
Genta Ananda Rakhsy <gentaananda48@gmail.com>
2024-03-30 00:41:26 +0700

    Merge branch andri into main

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({Key? key, required this.title});
 
   final String title;
 
@@ -39,7 +39,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         title: Text(
           'Al-Fatihah',
-          style: TextStyle(
+          style: GoogleFonts.poppins(
             fontWeight: FontWeight.bold,
             color: Color(0xFF672CBC),
             fontSize: 20.0,
@@ -55,44 +55,148 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
       ),
       body: ListView(
-        padding: EdgeInsets.all(16),
+        padding: const EdgeInsets.all(12.0),
         children: [
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: SizedBox(
+                height: 300.0,
+                width: double.infinity,
+                child: Stack(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            offset: const Offset(2.0, 2.0),
+                            blurRadius: 4.0,
+                            spreadRadius: 1.0,
+                          ),
+                          BoxShadow(
+                            color: Colors.white.withOpacity(0.5),
+                            offset: const Offset(-2.0, -2.0),
+                            blurRadius: 4.0,
+                            spreadRadius: 1.0,
+                          ),
+                        ],
+                        gradient: const LinearGradient(
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                          colors: [
+                            Color.fromARGB(255, 223, 200, 255), // ungu muda
+                            Color.fromARGB(255, 193, 140, 230), // ungu agak tua
+                          ],
+                        ),
+                      ),
+                      child: Stack(
+                        children: [
+                          Positioned(
+                            bottom: 9, // Geser ke bawah sedikit
+                            right: 9, // Geser ke kanan sedikit
+                            child: Opacity(
+                              opacity:
+                                  0.2, // Ubah ke transparansi yang diinginkan (0.0 - 1.0)
+                              child: Image.asset(
+                                'assets/alquran.png', // Ganti dengan path gambar Al-Qur'an Anda
+                                width: 180, // Sesuaikan ukuran gambar
+                              ),
+                            ),
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.all(16.0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "Al-Fatihah",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 30,
+                                    color: Colors.white,
+                                  ),
+                                  textAlign: TextAlign.center,
+                                ),
+                                SizedBox(height: 8),
+                                Text(
+                                  "The Opening",
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.white,
+                                  ),
+                                  textAlign: TextAlign.center,
+                                ),
+                                SizedBox(height: 8),
+                                Divider(),
+                                SizedBox(height: 8),
+                                Text(
+                                  "MECCAN • 7 VERSES",
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.white,
+                                  ),
+                                  textAlign: TextAlign.center,
+                                ),
+                                SizedBox(height: 8),
+                                Text(
+                                  "بِسْمِ اللّٰهِ الرَّحْمٰنِ الرَّحِيْمِ",
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.white,
+                                    fontFamily: 'Arabic',
+                                  ),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
           _ayatItem(
             nomor: 1,
-            arabic: 'مٰلِكِ يَوْمِ الدِّيْنِ',
+            arabic: 'مٰلِكِ يَوْمِ الدِّيْنِ',
             translation: 'Segala puji bagi Allah, Tuhan semesta alam.',
           ),
           _ayatItem(
             nomor: 2,
-            arabic: 'اَلْحَمْدُ لِلّٰهِ رَبِّ الْعٰلَمِيْنَۙ',
+            arabic: 'اَلْحَمْدُ لِلّٰهِ رَبِّ الْعٰلَمِيْنَۙ',
             translation:
                 'Segala Puji Bagi Allah Yang Maha Pengasih lagi Maha Penyayang.',
           ),
           _ayatItem(
             nomor: 3,
-            arabic: 'الرَّحْمٰنِ الرَّحِيْمِۙ',
+            arabic: 'الرَّحْمٰنِ الرَّحِيْمِۙ',
             translation: 'Yang Maha Pengasih lagi Maha Penyayang',
           ),
           _ayatItem(
             nomor: 4,
-            arabic: 'مٰلِكِ يَوْمِ الدِّيْنِ',
+            arabic: 'مٰلِكِ يَوْمِ الدِّيْنِ',
             translation: 'Pemilik hari pembalasan.',
           ),
           _ayatItem(
             nomor: 5,
-            arabic: 'اِيَّاكَ نَعْبُدُ وَاِيَّاكَ نَسْتَعِيْنُ',
+            arabic: 'اِيَّاكَ نَعْبُدُ وَاِيَّاكَ نَسْتَعِيْنُ',
             translation:
                 'Hanya kepada Engkaulah kami menyembah dan hanya kepada Engkaulah kami mohon pertolongan.',
           ),
           _ayatItem(
             nomor: 6,
-            arabic: 'اهْدِنَا الصِّرَاطَ الْمُسْتَقِيمَ',
+            arabic: 'اهْدِنَا الصِّرَاطَ الْمُسْتَقِيمَ',
             translation: 'Bimbinglah kami ke jalan yang lurus',
           ),
           _ayatItem(
             nomor: 7,
             arabic:
-                'صِرَاطَ الَّذِينَ أَنْعَمْتَ عَلَيْهِمْ غَيْرِ الْمَغْضُوبِ عَلَيْهِمْ وَلاَ الضَّالِّينَ',
+                'صِرَاطَ الَّذِينَ أَنْعَمْتَ عَلَيْهِمْ غَيْرِ الْمَغْضُوبِ عَلَيْهِمْ وَلاَ الضَّالِّينَ',
             translation:
                 'Jalan orang yang Kau beri nikmat kepada mereka, bukan (jalan) mereka yang dimurka, dan bukan (jalan) mereka yang tersesat',
           ),
@@ -210,18 +314,22 @@ class DataSearch extends SearchDelegate<String> {
   }
 
   Widget buildSearchResults(BuildContext context) {
+    // Buat daftar saran pencarian (suggestion list)
     final suggestionList = [];
 
+    // Filter daftar saran berdasarkan query
     final filteredList = suggestionList
         .where((text) => text.toLowerCase().contains(query.toLowerCase()))
         .toList();
 
+    // Tampilkan daftar hasil pencarian
     return ListView.builder(
       itemCount: filteredList.length,
       itemBuilder: (BuildContext context, int index) {
         return ListTile(
           title: Text(filteredList[index]),
           onTap: () {
+            // Tampilkan hasil
             showResults(context);
           },
         );
